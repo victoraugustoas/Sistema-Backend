ca0c9546e0f4c8797874661f5aad0bd2b0403279
Victor Augusto Andrade Silva <vaas@icomp.ufam.edu.br>
2019-04-14 09:33:51 -0400

    Incluido o Cors para uso externo do backend
    > Adicionado o cors como middleware para uso externo do backend
    
    > Adicionado alguns comentários

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,10 +1,16 @@
 const bodyParser = require('body-parser')
 const { captilizeLetter } = require('../utils/utils')
+const cors = require('cors')
 
 module.exports = (app) => {
 
+    // habilita a api para uso externo
+    app.use(cors())
+
+    // faz o parser dos objetos json
     app.use(bodyParser.json())
 
+    // se possuir titulo e descrição, coloca a primeira letra maiúscula
     app.use((req, res, next) => {
 
         if (req.body.title && req.body.description) {
