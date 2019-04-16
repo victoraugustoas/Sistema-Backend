22db3fdb19868b6210c500189454ced93503de7d
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-04-16 18:45:09 -0400

    Adicionada conexão com banco de dados de produção

--- config/db.js
+++ config/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { useNewUrlParser: true })
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongodbatlas-ymqp4.mongodb.net/test?retryWrites=true/${process.env.DB_NAME}`, { useNewUrlParser: true })
+} else {
+    mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { useNewUrlParser: true })
+}
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
