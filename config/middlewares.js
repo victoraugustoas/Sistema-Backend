4856b85247aaa6a37eab8919b00406e1a1212443
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-04-18 01:20:22 -0400

    Problema de cache
    > http 304

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,9 +11,9 @@ module.exports = (app) => {
 
     // Removendo cache
     app.use((req, res, next) => {
-        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
-        res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
-        res.setHeader("Expires", "0"); // Proxies.
+        res.header("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        res.header("Pragma", "no-cache"); // HTTP 1.0.
+        res.header("Expires", "0"); // Proxies.
         next()
     })
 
