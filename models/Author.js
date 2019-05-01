109d816906efa5b0fbf639bdb7a7a9c6aba0ba82
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-05-01 18:17:55 -0400

    Novo model de autor de posts

---
+++ models/Author.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const authorSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Authors', authorSchema)
